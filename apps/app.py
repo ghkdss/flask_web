75a77296bb34773a746d0c424b71bcc29d976222
ghkdss <sotjstod@gmail.com>
2025-09-24 11:52:12 +0900

    0924-4

--- apps/app.py
+++ apps/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from flask_login import LoginManager
 
 from apps.config import config
 import os
@@ -10,6 +11,7 @@ config_key = os.environ.get("FLASK_CONFIG_KEY")
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+login_manager = LoginManager()
 
 def create_app():
   app = Flask(__name__)
@@ -19,11 +21,14 @@ def create_app():
   csrf.init_app(app)
   db.init_app(app)
   Migrate(app, db)
+  login_manager.init_app(app)
 
   from apps.crud import views as crud_views
   from apps.study import views as study_views
+  from apps.auth import views as auth_views
 
   app.register_blueprint(crud_views.crud, url_prefix='/crud')
   app.register_blueprint(study_views.study, url_prefix="/study")
+  app.register_blueprint(auth_views.auth, url_prefix='/auth')
 
   return app
